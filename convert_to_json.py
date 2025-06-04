0e08d591bff0ac4a3ead300e6c486f5802f1a447
tamar-levi <t0527144636@gmail.com>
2025-06-04 19:53:52 +0300

    Second Comnit

--- convert_to_json.py
+++ convert_to_json.py
@@ -1,6 +1,47 @@
+
+
+import openai
 import json
 import os
+from dotenv import load_dotenv
 from transformers import pipeline
+
+#
+# load_dotenv()
+# openai.api_key = os.getenv("OPENAI_API_KEY")
+#
+# def generate_specifications(text_description):
+#     prompt = f"""
+#         You receive the content of a complete construction document and you need to extract and summarize all structural parameters - such as material types, thicknesses, usage classifications, and compliance standards - from that document into clean JSON.
+#         Reply in JSON format only No further comments!
+#         The content of the document is:
+#         {text_description}
+#     """
+#
+#     response = openai.chat.completions.create(
+#         model="gpt-4",
+#         messages=[
+#             {"role": "user", "content": prompt}
+#         ],
+#         temperature=0
+#     )
+#     content = response.choices[0].message.content.strip()
+#     try:
+#         result_json = json.loads(content)
+#     except json.JSONDecodeError:
+#         return None
+#
+#     return result_json
+#
+# if __name__ == "__main__":
+#     f = open("construction specification.txt", 'r', encoding='utf-8')
+#     description = f.readlines()
+#     result = generate_specifications(description)
+#     if result:
+#         with open("results/specifications.json", "w") as f:
+#             json.dump(result, f, indent=2)
+#         print("✅ specifications.json created")
+
 
 with open(r"C:\Users\1\Downloads\for_test.txt", "r", encoding="utf-8") as f:
     spec_text = f.read()
@@ -12,8 +53,8 @@ Please extract and convert the following construction specification into structu
 Each entry should include:
 component name
 materials
-dimensions (if available)
-classification (e.g. wall, floor, ceiling)
+dimensions 
+classification 
 any standards mentioned
 
 TEXT:
